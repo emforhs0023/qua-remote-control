9d93ebd56b21350bbf6f07e17faf65d5755024fb
Jjini <emforhs0023@naver.com>
2020-01-10 20:18:03 +0900

    commit

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -9,6 +9,12 @@ router.get('/', (req, res, next) => {
     res.render('main');
 });
 
+router.get('/1', (req, res, next) => {
+    res.render('main1');
+});
+router.get('/2', (req, res, next) => {
+    res.render('pump');
+});
 // 웰태그
 router.post('/dataInfo', (req, res, next) => {
     try{
@@ -27,6 +33,38 @@ router.post('/dataInfo', (req, res, next) => {
                 MQTTPublish.start(header, uid, action, part, timestart, timestop)
             }
         }
+        
+        
+    }catch(e){
+        next(e)
+    }
+})
+
+router.post('/dataInfo1', (req, res, next) => {
+    try{
+        let header = req.body.header
+        let uid = req.body.uid
+        let action = req.body.sub.action
+        let part = req.body.sub.part
+        
+        // let timestart = req.body.timestart
+        // let timestop = req.body.timestop
+        MQTTPublish.start(header, uid, action, part)
+        // if(req.body.length == undefined) {
+        //      console.log(req.body)
+        // } else {
+        //     for(var i=0; i<req.body.length; i++) {
+        //         let header = req.body[i].header
+        //         let uid = req.body[i].uid
+        //         let action = req.body[i].sub.action
+        //         let part = req.body[i].sub.part
+        //         let timestart = req.body[i].timealarm.timestart
+        //         let timestop = req.body[i].timealarm.timestop
+                
+        //         // console.log(sub.action)
+        //         MQTTPublish.start(header, uid, action, part, timestart, timestop)
+        //     }
+        // }
         
         
     }catch(e){
